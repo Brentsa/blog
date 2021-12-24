22eefbba59be8990a2e5c9e076b3f7adfa2377d1
Brentsa <seanbrent5@gmail.com>
2021-12-23 21:25:08 -0500

    simultaneous filtering with search and category

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -13,7 +13,11 @@
         All
     </x-dropdown-item>
     @foreach ($categories as $category)
-        <x-dropdown-item href="/?category={{$category->slug}}" :active="isset($currentCategory) && $currentCategory->is($category)">
+        <x-dropdown-item 
+            {{-- take the request queries except category, and build them as a string --}}
+            href="/?category={{$category->slug}}&{{http_build_query(request()->except('category'))}}" 
+            :active="isset($currentCategory) && $currentCategory->is($category)"
+        >
             {{ucwords($category->name)}}
         </x-dropdown-item>
     @endforeach
