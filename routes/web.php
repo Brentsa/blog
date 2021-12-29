37dee40458393857de9997be26256d916da436f0
Brentsa <seanbrent5@gmail.com>
2021-12-29 18:49:54 -0500

    users can now subscribe to the newsletter by providing their email

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 //Route to test mailchimp functionality
-Route::get('/ping', function (){
+Route::post('/newsletter', function (){
+
+    request()->validate([ 'email' => ['required', 'email']]);
+
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -18,16 +21,23 @@ Route::get('/ping', function (){
         'server' => 'us20'
     ]);
 
-    // Add a member to the list 
-    // $response = $mailchimp->lists->addListMember("83b97a4c49",[
-    //     'email_address' => 'seanbrent5@gmail.com',
-    //     'status' => 'subscribed'
-    // ]);
-
+    try{
+        // Add a member to the list 
+        $response = $mailchimp->lists->addListMember("83b97a4c49",[
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    }
+    catch(Exception $e){
+        throw Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter.'
+        ]);
+    }
+    
     // Get the list info
-    $response = $mailchimp->lists->getListMembersInfo("83b97a4c49");
+    //$response = $mailchimp->lists->getListMembersInfo("83b97a4c49");
 
-    dd($response);
+    return redirect('/')->with('success', 'You have successfully subscribed.');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
