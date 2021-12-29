33d6e0c2259c14f8f8c8e7199b1d5245a80474bb
Brentsa <seanbrent5@gmail.com>
2021-12-29 07:57:43 -0500

    created post comment form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,16 +1,18 @@
 @props(['comment'])
-<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60?u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
-    </div>
+<x-panel class="bg-gray-100">
+    <article class="flex bg-gray-100 space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
+        </div>
 
-    <div>
-        <h3 class="font-bold">{{$comment->author->name}}</h3>
-        <p class="text-xs mb-4">
-            Posted <time>{{$comment->created_at->diffForHumans()}}</time>
-        </p>
-        <p>
-            {{$comment->body}}
-        </p>
-    </div>
-</article>
+        <div>
+            <h3 class="font-bold">{{$comment->author->name}}</h3>
+            <p class="text-xs mb-4">
+                Posted <time>{{$comment->created_at->diffForHumans()}}</time>
+            </p>
+            <p>
+                {{$comment->body}}
+            </p>
+        </div>
+    </article>
+</x-panel>
